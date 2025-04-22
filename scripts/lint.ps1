974bdf3bb13467d435c91253ab52719c206a503b
musubituwani <musubituwani@gmail.com>
2025-04-22 10:30:29 +0200

    Delete scripts directory

--- scripts/lint.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Running Java linter..."
-# Simulate a linter
-Get-ChildItem -Recurse -Include *.java | ForEach-Object {
-    Write-Host "Checking file: $($_.FullName)"
-}
-Write-Host "Linting complete!"
